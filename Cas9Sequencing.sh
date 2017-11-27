b999050c487e5788f68c337ce9c3eb0e3c96687d
zf <fzhang1117@mpimp-golm.mpg.de>
2017-11-27 18:53:05 +0100

    zf

--- Cas9Sequencing.sh
+++ Cas9Sequencing.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+pattern=$1
+length=${#pattern}
+
 cd ./fastq
 
 gunzip *
@@ -8,8 +11,8 @@ for seq in $(ls *fq | sed 's/.fq//g')
 do
 	echo $seq
 	#fastx_quality_stats -i $seq.fq -o ../out/summary_$seq.txt -Q 33
-	python ../pattern_extract.py $seq.fq ../fl_result/extract/${seq}.extract.txt ../fl_result/amazing/${seq}_amazing.txt "GACCTT[ATCG]{24}TTTTGG" > ../log/${seq}_log.txt
-	python ../result_stat.py ../fl_result/extract/${seq}.extract.txt 36 ../fl_result/summary/${seq}_summary.txt
+	python ../pattern_extract.py $seq.fq ../fl_result/extract/${seq}.extract.txt ../fl_result/amazing/${seq}_amazing.txt $pattern > ../log/${seq}_log.txt
+	python ../result_stat.py ../fl_result/extract/${seq}.extract.txt $length ../fl_result/summary/${seq}_summary.txt
 	awk '{a[$1]++}END{for(i in a){print i, a[i] | "sort -r -n -k 2"}}' ../fl_result/extract/${seq}.extract.txt > ../fl_result/count/${seq}_count.txt
 done
 
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-## this is a brief introduce of the pipeline ##
+# Cas9Sequencing for handle Cas9 DNA sequencing data
+
+This program is a short pipeline in handle Cas9 DNA sequencing data
+
+You should put your fastq data in dir ./fastq first, end by .fq.gz or .fq
+
+Then type:
+```sh
+sh Cas9Sequencing.sh
+```
